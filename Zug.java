c11b8bc6cf7226510908f2e9a24a1c0bce7d48d9
jotaro <florian.gasior@gmail.com>
2026-05-23 14:22:17 +0200

    Update Zug.java

--- Zug.java
+++ Zug.java
@@ -3,12 +3,11 @@ public class Zug extends Gegner{
 Rectangle ZugBox;
 int Geschwindigkeit=80;
 int XPosition=800;
-public void Zug()
+public Zug()
 {
-    ZugBox= new Rectangle(XPosition, YPosition, 120, 100)
+    ZugBox= new Rectangle(XPosition, YPosition, 120, 100);
     try {
-    Thread.sleep(((int)(Math.random())*10000)+4000);
-    } 
+    Thread.sleep((int)(Math.random() * 10000) + 4000);
     catch (InterruptedException e) {
         e.printStackTrace();
     }
