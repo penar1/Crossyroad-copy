283f00d6a391a3c5f4f61392325fb6c53d86d9f1
jotaro <florian.gasior@gmail.com>
2026-05-23 09:30:56 +0200

    s

--- World.java
+++ World.java
@@ -2,15 +2,13 @@ public class World {
 
   Lanes[] Lane = new Lanes[5];
 
-public void World()
-{
-for(int i=0; i<Lane.length; i++)
-  {
+public void World() {
+  for (int i = 0; i < Lane.length; i++) {
     Lane[i] = new Lanes();
-    Lane[i].Lanes(0, 120+i*120);
-    Lane[i].settype((int)(Math.random()*3));
+    Lane[i].Lanes(0, 120 + i * 120);
+    Lane[i].settype((int) (Math.random() * 3));
   }
-
+}
   public void LaneGen()
   {
     for(int i = Lane.length - 1; i > 0; i--) {
@@ -20,6 +18,6 @@ for(int i=0; i<Lane.length; i++)
     }
 
   }
-}  
+
 
 }
