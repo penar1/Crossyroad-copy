82c110d5c00013d2bf227a0e34f1d79f435aff20
jotaro <florian.gasior@gmail.com>
2026-05-23 14:04:16 +0200

    Update Zug.java

--- Zug.java
+++ Zug.java
@@ -1,2 +1,12 @@
 public class Zug extends Gegner{
+
+Rectangle ZugBox;
+int Geschwindigkeit=80;
+int XPosition=800;
+public void Zug()
+{
+
+    ZugBox= new Rectangle(XPosition, YPosition, 120, 100)
+    
+}
 }
