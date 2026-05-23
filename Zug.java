d53c006af9f1bf65e181f922b41dfefc41a573c3
jotaro <florian.gasior@gmail.com>
2026-05-23 14:18:41 +0200

    Update Zug.java

--- Zug.java
+++ Zug.java
@@ -5,10 +5,9 @@ int Geschwindigkeit=80;
 int XPosition=800;
 public void Zug()
 {
-
     ZugBox= new Rectangle(XPosition, YPosition, 120, 100)
     try {
-    Thread.sleep(((int)(Math.random)*10000)+4000);
+    Thread.sleep(((int)(Math.random())*10000)+4000);
     } 
     catch (InterruptedException e) {
         e.printStackTrace();
