238d308ede0ec43caf188b2dc17a7d6b0fedbba1
jotaro <florian.gasior@gmail.com>
2026-05-23 14:42:15 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -6,11 +6,22 @@ public class Player {
     int speed;
     static Rectangle hitbox;
     int score=0;
+    boolean Collision=false;
 
     public Player(int x, int y) {
         hitbox = new Rectangle(x, y, 50, 50);
     }
-
+    public boolean CollisionCheck()
+    {
+        if(hitbox.intersects(Zug.ZugBox))
+        {
+            boolean Collision=true;
+        }
+        if(hitbox.intersects(Auto.Rectangle))
+        {
+            boolean Collision=true;
+        }
+    }
     public void bewegen() {
         score = score+1;
         hitbox.y -= 60;
